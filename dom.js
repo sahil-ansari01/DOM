73b841c2179ee784001aee95c77b59990a2f0746
sahil-ansari01 <opensourcecarrier@gmail.com>
2023-12-11 19:26:02 +0530

    DOM task 6

--- dom.js
+++ dom.js
@@ -48,14 +48,53 @@
 
 // getElementsByTagName 
 
-const li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent = 'Hello 2';
-li[1].style.fontWeight = 'bold';
-li[1].style.backgroundColor = 'yellow';
-li[2].style.background = 'green';
+// const li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+// li[2].style.background = 'green';
 
-for (let i = 0; i < li.length; i++) {
-    li[i].style.fontWeight = 'bold';
+// for (let i = 0; i < li.length; i++) {
+//     li[i].style.fontWeight = 'bold';
+// }
+
+
+// querySelector
+
+const header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+const input = document.querySelector('input');
+input.value = 'Hello World';
+
+const submit = document.querySelector('input[type="submit"]');
+submit.value = 'SEND'
+
+const item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+const lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';  
+
+const secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';  
+
+const thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility = 'hidden';  
+
+
+// querySelectorAll 
+const titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+const li = document.querySelectorAll('li');
+li[1].style.color = 'green';
+
+const odd = document.querySelectorAll('li:nth-child(odd)');
+
+for (let i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = 'green';
 }
