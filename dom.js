01247e32930f66daa192140f7a69fbb6761baf87
sahil-ansari01 <opensourcecarrier@gmail.com>
2023-12-11 18:45:07 +0530

    learnt DOM

---
+++ dom.js
@@ -0,0 +1,36 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 121;
+// console.log(document.doctype)
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms);
+// console.log(document.links);
+
+// GETELEMENTBYID
+// console.log(document.getElementById('header-title'));
+
+// const headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerHTML = "Goodbye";
+
+// console.log(headerTitle.innerText)
+
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+
+// changing style
+const header = document.getElementById('main-header');
+header.style.border = 'solid 3px #000';
+
+// GetElementByClassName
+
+const addItems = document.getElementById('addItems');
+addItems.style.fontWeight = "900";
+addItems.style.color = 'green';
+// const items = document.getElementsByClassName('list-group-item');
+// console.log(items);
