86600ab973e03e874e87f08191912da2be34b979
sahil-ansari01 <opensourcecarrier@gmail.com>
2023-12-12 20:15:50 +0530

    UI changes for list

--- index.js
+++ index.js
@@ -28,19 +28,20 @@ function addItem(e) {
     // append 
     li.appendChild(textNode);
 
-    // Create edit button element
-    const edit = document.createElement('button');
-    edit.className = 'btn btn-sm float-right edit';
-    edit.setAttribute('style', 'margin-left: 15px;');
-    edit.appendChild(document.createTextNode('Edit'));
-    li.appendChild(edit);
-    
     // Create del button element
     const del = document.createElement('button');
     del.className = 'btn btn-danger btn-sm float-right delete';
     del.appendChild(document.createTextNode('X'));
     li.appendChild(del);
 
+    // Create edit button element
+    const edit = document.createElement('button');
+    edit.className = 'btn btn-sm float-right edit';
+    edit.setAttribute('style', 'margin-right: 20px;');
+    edit.appendChild(document.createTextNode('Edit'));
+    li.appendChild(edit);
+    
+   
     
     // append li to list
     itemList.appendChild(li);
@@ -58,6 +59,20 @@ function removeItem(e) {
 }
 
 // Filter items
-function filterItems() {
+function filterItems(e) {
+    // convert text to lowercase
+    const text = e.target.value.toLowerCase();
 
+    // Get li's
+    const items = itemList.getElementsByTagName('li');
+
+    // convert to an array
+    Array.from(items).forEach(function(item){
+        const itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    })
 }
