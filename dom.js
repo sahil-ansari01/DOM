6e105a5d841acce46fa251eba71a8ea75981cc80
sahil-ansari01 <opensourcecarrier@gmail.com>
2023-12-12 17:58:28 +0530

    manipulate the DOM task 7

--- dom.js
+++ dom.js
@@ -86,15 +86,98 @@
 
 
 // querySelectorAll 
-const titles = document.querySelectorAll('.title');
-console.log(titles);
-titles[0].textContent = 'Hello';
+// const titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent = 'Hello';
 
-const li = document.querySelectorAll('li');
-li[1].style.color = 'green';
+// const li = document.querySelectorAll('li');
+// li[1].style.color = 'green';
 
-const odd = document.querySelectorAll('li:nth-child(odd)');
+// const odd = document.querySelectorAll('li:nth-child(odd)');
 
-for (let i = 0; i < odd.length; i++) {
-    odd[i].style.backgroundColor = 'green';
-}
+// for (let i = 0; i < odd.length; i++) {
+//     odd[i].style.backgroundColor = 'green';
+// }
+
+
+// Traversing the DOM
+
+const itemList = document.querySelector('#items');
+// parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode)
+
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement)
+
+// childNode 
+// console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// firstChild 
+// console.log(itemList.firstChild)
+// firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello';
+
+// lastChild 
+// console.log(itemList.lastChild)
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello';
+
+// nextSiblings
+// console.log(itemList.nextSibling);
+// nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+// previousElementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+// createElement
+
+// Create a div
+const newDiv = document.createElement('div');
+
+// Add class
+newDiv.className = 'hello';
+console.log(newDiv);
+
+// Add id
+newDiv.id = 'hello1';
+
+// add attribute
+newDiv.setAttribute('title', 'hello Div');
+
+// create text node
+const newDivText = document.createTextNode('Hello World');
+
+// add text to div
+newDiv.appendChild(newDivText)
+
+const container = document.querySelector('header .container');
+const h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '45px';
+
+container.insertBefore(newDiv, h1);
+
+// Create a list
+const newList = document.createElement('li');
+newList.className = 'list-group-item';
+newList.textContent = 'Hello World';
+
+console.log(newList);
+
+itemList.insertBefore(newList, itemList.firstElementChild);
